d3ef50ede7c01b8ee057ee1a33ddbcb43fef9e72
Nabil Hashmi <nabil2011@gmail.com>
2015-04-21 16:59:28 -0400

    Updated form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,57 @@
+# Devise method for creating guest users
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  protect_from_forgery
+
+  # if user is logged in, return current_user, else return guest_user
+  def current_or_guest_user
+    if current_user
+      if session[:guest_user_id] && session[:guest_user_id] != current_user.id
+        logging_in
+        guest_user(with_retry = false).try(:destroy)
+        session[:guest_user_id] = nil
+      end
+      current_user
+    else
+      guest_user
+    end
+  end
+
+  # find guest_user object associated with the current session,
+  # creating one as needed
+  def guest_user(with_retry = true)
+    # Cache the value the first time it's gotten.
+    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
+
+  rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
+     session[:guest_user_id] = nil
+     guest_user if with_retry
+  end
+
+  private
+
+  # called (once) when the user logs in, insert any code your application needs
+  # to hand off from guest_user to current_user.
+  # For example:
+    # guest_comments = guest_user.comments.all
+    # guest_comments.each do |comment|
+      # comment.user_id = current_user.id
+      # comment.save!
+    # end
+  def logging_in
+    guest_form = guest_user.form
+    guest_form.user_id = current_user.id
+    guest_form.save!
+  end
+    
+
+  def create_guest_user
+    user = User.create(:email => "guest_#{Time.now.to_i}#{rand(100)}@example.com")
+    user.save!(:validate => false)
+    session[:guest_user_id] = user.id
+    return user
+  end
+
+helper_method :current_or_guest_user
+
 end
--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,13 +1,29 @@
 class FormsController < ApplicationController
 
   before_action :find_form, only: [:show, :edit, :update, :destroy]
+  helper_method :current_or_guest_user
+  before_action :current_or_guest_user
 
   def show
+  # Calculate costs and display outputs 
+    # @form.tax_adjusted_income 
+    # @form.neighborhood_cost
+    # @form.healthcare_cost
+    # @form.savings_cost
+    # @dining_cost = @form.dining_cost(@form.dining_out_low, @form.dining_out_medium, @form.dining_out_high)
+    @dining_cost = @form["dining_out_low"]
+    # @form.cabs_cost
+    # @form.public_transportation_cost
+    # @form.driving_cost
+    # @form.travel_cost
+    # @form.gym_cost
+    # @form.recreation_cost
+    # @form.shopping_cost
+    # @form.total_cost
   end
 
   def new
-    @form = Form.new
-    # Menu options 
+    @form = current_or_guest_user.forms.new 
     @neighborhoods = Neighborhood.where(state: "DC").order(:name)
     @health_options = @form.healthcare_options
     # @form.savings_options
@@ -20,24 +36,12 @@ class FormsController < ApplicationController
     # @form.recreation_options
     # @form.shopping_options
 
-
-    # Calculate costs 
-    # @form.neighborhood_cost
-    # @form.healthcare_cost
-    # @form.savings_cost
-    # @form.eating_out_cost
-    # @form.cabs_cost
-    # @form.public_transportation_cost
-    # @form.driving_cost
-    # @form.travel_cost
-    # @form.gym_cost
-    # @form.recreation_cost
-    # @form.shopping_cost
   end
 
   def create
-    @form = Form.create(forms_params)
+    @form = current_or_guest_user.forms.new(forms_params)
     if @form.save
+      @form.update(neighborhood_id: params[:neighborhood])
       redirect_to @form
     else
       render :new
@@ -62,18 +66,21 @@ class FormsController < ApplicationController
 
     def forms_params
       params.require(:form).permit(
+        :income, 
         :neighborhood,
         :healthcare,
-        :savings,
-        :eating_out,
-        :groceries,
-        :cabs,
-        :public_transportation,
-        :driving,
-        :travel,
-        :gym,
-        :recreation,
-        :shopping
+        :savings
+        # :dining_out_low,
+        # :dining_out_medium,
+        # :dining_out_high,
+        # :driving_trips,
+        # :car_trip_duration,
+        # :mass_transit_trips,
+        # :groceries,
+        # :cabs,
+        # :public_transportation,
+        # :recreation,
+        # :shopping
       )
     end
 
