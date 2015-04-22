deb9949f097301a1a4e493de4c2a4eaab8011e17
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-04-22 12:15:55 -0400

    housing data

--- db/migrate/20150421133343_create_neighborhoods.rb
+++ db/migrate/20150421133343_create_neighborhoods.rb
@@ -1,15 +1,15 @@
 class CreateNeighborhoods < ActiveRecord::Migration
   def change
     create_table :neighborhoods do |t|
-      # t.string :name
-      # t.decimal :studio_price
-      # t.decimal :one_bed_price
-      # t.decimal :two_bed_price
       t.string :name
+      t.integer :studio_price
+      t.integer :one_bed_price
+      t.iteger :two_bed_price
+      # t.string :name
       t.string :city
       t.string :state
-      t.string :metro
-      t.string :county_name
+      # t.string :metro
+      # t.string :county_name
       t.timestamps null: false
       t.belongs_to :form
     end
